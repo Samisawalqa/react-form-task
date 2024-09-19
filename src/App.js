ce1c5c648c3e12a86caee305a8c42b5b685318d9
ikroupie <168979831+ikroupie@users.noreply.github.com>
2024-09-19 11:14:22 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import StateLogin from "./components/StateLogin";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <StateLogin />
+      </main>
+    </>
+  );
+}
+
+export default App;
